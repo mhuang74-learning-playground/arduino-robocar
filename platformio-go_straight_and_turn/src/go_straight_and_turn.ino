1d7e9367e3b9e42ba7dc2ccc4c13d3c4487510a0
Michael Huang <mhuang74@gmail.com>
2022-12-11 18:47:03 +0800

    Use IR sensor to avoid obstacles

--- platformio-go_straight_and_turn/src/go_straight_and_turn.ino
+++ platformio-go_straight_and_turn/src/go_straight_and_turn.ino
@@ -1,27 +1,37 @@
 // Define output pins to L293D HBridge
 
-int Left_motor_forward = 9;     // Left Motor Forward (IN2)
-
+const int Left_motor_forward = 9;     // Left Motor Forward (IN2)
 // pin 8 not working , so use 6 instead
-// left motor wiring reversed, so IN1 is Reverse
-int Left_motor_reverse = 6;     // Left Motor Reverse (IN1)
+// left motor wiring reversed, so IN1 is Reverse instead of IN2
+const int Left_motor_reverse = 6;     // Left Motor Reverse (IN1)
+const int Right_motor_forward = 10;    // Right Motor Forward (IN3)
+const int Right_motor_reverse = 11;    // Right Motor Reverse (IN4)
 
-int Right_motor_forward = 10;    // Right Motor Forward (IN3)
+// Define input pins for IR Obstacle Detector
+const int Left_ir_detector = 3; // P3.4 OUT3
+const int Right_ir_detector = 4; // P3.5 OUT4
 
-int Right_motor_reverse = 11;    // Right Motor Reverse (IN4)
+// define motor driver RMS to go straight
+const int Right_rms = 102;
+const int Left_rms = 100;
 
 void setup()
 {
 
+  Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
+
   pinMode(LED_BUILTIN, OUTPUT);
 
   pinMode(Left_motor_forward, OUTPUT);
-
   pinMode(Left_motor_reverse, OUTPUT);
-
   pinMode(Right_motor_forward, OUTPUT);
-
   pinMode(Right_motor_reverse, OUTPUT);
+
+  pinMode(Right_ir_detector, INPUT);
+  pinMode(Left_ir_detector, INPUT);
+
+  // rapid blink to signal START
+  signal(1000, 20);
 
 }
 
@@ -49,8 +59,8 @@ void stop()
   digitalWrite(Left_motor_forward, LOW); 
   digitalWrite(Left_motor_reverse, LOW);
 
-  // 2 rapid blinks to signal stop
-  signal(100, 2);
+  // 1 blinks to signal stop
+  signal(10, 1);
 }
 
 /*
@@ -60,44 +70,68 @@ void forward(int duration)
 {
   // Engage right motor
   digitalWrite(Right_motor_forward, HIGH);  // Right Motor Forward
-  analogWrite(Right_motor_forward, 105); // PWM 0-255. Adjust to compensate skew.
+  analogWrite(Right_motor_forward, Right_rms); // PWM 0-255. Adjust to compensate skew.
   digitalWrite(Right_motor_reverse, LOW);    
   analogWrite(Right_motor_reverse, 0);
                    
 
   // Engage Left Motor
   digitalWrite(Left_motor_forward, HIGH);  // Left Motor Forward
-  analogWrite(Left_motor_forward, 100); // PWM 0-255. Adjust to compensate skew.
+  analogWrite(Left_motor_forward, Left_rms); // PWM 0-255. Adjust to compensate skew.
   digitalWrite(Left_motor_reverse, LOW);
   analogWrite(Left_motor_reverse, 0);
 
   // Blink LED while going forward
-  signal(duration, duration/200);
+  signal(duration, duration/100);
 
   stop();
 }
 
 /*
-* Turn RIGHT in place
 * duration - in msec
 */
-void turnRight(int duration)
+void reverse(int duration)
 {
   // Engage right motor
   digitalWrite(Right_motor_forward, LOW);  // Right Motor Forward
   analogWrite(Right_motor_forward, 0); // PWM 0-255. Adjust to compensate skew.
   digitalWrite(Right_motor_reverse, HIGH);    
-  analogWrite(Right_motor_reverse, 105);
+  analogWrite(Right_motor_reverse, Right_rms);
+                   
+
+  // Engage Left Motor
+  digitalWrite(Left_motor_forward, LOW);  // Left Motor Forward
+  analogWrite(Left_motor_forward, 0); // PWM 0-255. Adjust to compensate skew.
+  digitalWrite(Left_motor_reverse, HIGH);
+  analogWrite(Left_motor_reverse, Left_rms);
+
+  // Blink LED while going forward
+  signal(duration, duration/100);
+
+  stop();
+}
+
+/*
+* SPIN RIGHT - turn right in place
+* duration - in msec
+*/
+void spinRight(int duration)
+{
+  // Engage right motor
+  digitalWrite(Right_motor_forward, LOW);  // Right Motor Forward
+  analogWrite(Right_motor_forward, 0); // PWM 0-255. Adjust to compensate skew.
+  digitalWrite(Right_motor_reverse, HIGH);    
+  analogWrite(Right_motor_reverse, Right_rms);
                    
 
   // Engage Left Motor
   digitalWrite(Left_motor_forward, HIGH);  // Left Motor Forward
-  analogWrite(Left_motor_forward, 100); // PWM 0-255. Adjust to compensate skew.
+  analogWrite(Left_motor_forward, Left_rms); // PWM 0-255. Adjust to compensate skew.
   digitalWrite(Left_motor_reverse, LOW);
   analogWrite(Left_motor_reverse, 0);
 
   // Blink LED while turning
-  signal(duration, duration/300);
+  signal(duration, duration/125);
 
   stop();
 }
@@ -105,16 +139,32 @@ void turnRight(int duration)
 void loop()
 {
 
-  delay(100);
-  
-  // rapid blink to signal start
-  signal(300, 10);
-  
-  delay(100);
+  int blocked_right = digitalRead(Right_ir_detector) == LOW;
+  int blocked_left = digitalRead(Left_ir_detector) == LOW;
 
-  // go forward
-  forward(1000);
+  // if detect blockage, stop and take reading again
+  if (blocked_right || blocked_left) {
+    stop();
+    signal(200, 10);
+    blocked_right = digitalRead(Right_ir_detector) == LOW;
+    blocked_left = digitalRead(Left_ir_detector) == LOW;
+  }
 
-  turnRight(300);
+  if (blocked_right && blocked_left) {
+    Serial.println("Both RIGHT and LEFT Blocked!");
+    reverse(200);
+    spinRight(400);
+  } else if (blocked_right) {
+    Serial.println("RIGHT Blocked!");
+    reverse(200);
+    spinRight(700);
+  } else if (blocked_left) {
+    Serial.println("LEFT Blocked!");
+    reverse(200);
+    spinRight(300);
+  } else {
+    // go forward
+    forward(200);
+  }
 
 }
